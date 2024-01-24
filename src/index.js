e650dc8f817cf234b3859c7fe039276f71bd144f
takumi.f.ito <takumi.f.ito@rakuten.com>
2024-01-25 03:03:06 +0900

    add publicURL

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
